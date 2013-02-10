030c0297dddf83dacd5ad641d4b0ca3e2d3d75b8
Michael Graessle <mike@graessle.net>
2013-02-10 09:59:09 -0600

    PID Settings

--- RaspberryPi/pid.c
+++ RaspberryPi/pid.c
@@ -1,5 +1,5 @@
 /*
-    Copyright 2009-2012
+ Copyright 2009-2012
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
@@ -15,77 +15,58 @@
  
  */
 
-
-
 #include "pid.h"
 
+void setupPid(Pid * da) {
+//  da->Kp = 21.1263;
+//  da->Kd = 1521.1382;
+//  da->Ki = 0.2772;
+//  da->updateInt = 8;
 
+	da->Kp = 78.2577;
+	da->Kd = 269.7192;
+	da->Ki = 0.9532;
+	da->updateInt = 1;
 
-void setupPid(Pid * da){
-  // Old Tune
-  // Kp: 10.5715 Kd: 299.8764 Ki: 0.0020 Time: 07:01 overShoot: 0.4032 finalError: 0.0005
-  // Tune with random read errors and 30 point history
-  // double kp = 5.9279, kd = 156.6051, ki = 0.0015;
-  // Tune with random read errors and 10 point history, Random target/abiemt
-  //  kp=4.5904, kd= 102.3394, ki=  0.0025
-  //  kp=3.3242, kd= 5.9397, ki=  0.0026
-
-  da->Kp = 21.1263;
-  da->Kd = 1521.1382;
-  da->Ki = 0.2772;
-  da->updateInt = 8;
-
-  da->minOutput = 0;
-  da->maxOutput = 100;
-  da->previousError = 0;
-  da->integral = 0;
+	da->minOutput = 0;
+	da->maxOutput = 100;
+	da->previousError = 0;
+	da->integral = 0;
 }
-
 
 void setMaxDuty(Pid * da, int maxDuty) {
-  da->maxOutput = maxDuty;
+	da->maxOutput = maxDuty;
 }
-
-
 
 int getDutyFromAdjuster(Pid * pid, double targetTemp, double currentTemp) {
 
-  if (pid->second % pid->updateInt == 0) {
+	if (pid->second % pid->updateInt == 0) {
 
-    float dt = pid->updateInt;
+		float dt = pid->updateInt;
 
-    float error = targetTemp - currentTemp;
-    pid->integral = pid->integral + (error * dt * pid->Ki);
+		float error = targetTemp - currentTemp;
+		pid->integral = pid->integral + (error * dt * pid->Ki);
 
-    if (pid->integral > pid->maxOutput) {
-      pid->integral = pid->maxOutput;
-    }
-    else if (pid->integral < pid->minOutput) {
-      pid->integral = pid->minOutput;
-    }
+		if (pid->integral > pid->maxOutput) {
+			pid->integral = pid->maxOutput;
+		} else if (pid->integral < pid->minOutput) {
+			pid->integral = pid->minOutput;
+		}
 
-    float derivative = (error - pid->previousError) / dt;
-    float newOutput = (pid->Kp * error) + (pid->integral) + (pid->Kd * derivative);
-    pid->previousError = error;
+		float derivative = (error - pid->previousError) / dt;
+		float newOutput = (pid->Kp * error) + (pid->integral) + (pid->Kd * derivative);
+		pid->previousError = error;
 
-    if (newOutput >= pid->maxOutput) {
-      newOutput = pid->maxOutput;
-    }
-    else if (newOutput <= pid->minOutput) {
-      newOutput = pid->minOutput;
-    }
-    pid->output = (int) newOutput;
-  }
+		if (newOutput >= pid->maxOutput) {
+			newOutput = pid->maxOutput;
+		} else if (newOutput <= pid->minOutput) {
+			newOutput = pid->minOutput;
+		}
+		pid->output = (int) newOutput;
+	}
 
-  pid->second++;
-  return pid->output;
+	pid->second++;
+	return pid->output;
 
 }
-
-
-
-
-
-
-
 
