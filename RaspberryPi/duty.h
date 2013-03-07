79577bca73348eaaa9a11a2baa67b78c864fbb76
Michael Graessle <mike@graessle.net>
2013-03-06 19:33:20 -0600

    Copyright headers

--- RaspberryPi/duty.h
+++ RaspberryPi/duty.h
@@ -1,5 +1,5 @@
 /*
-    Copyright 2009-2011 Michael Graessle
+    Copyright 2009-2013 Michael Graessle
  
  
  This program is free software: you can redistribute it and/or modify
--- RaspberryPi/pid.c
+++ RaspberryPi/pid.c
@@ -1,5 +1,5 @@
 /*
- Copyright 2009-2012
+ Copyright 2009-2013
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
