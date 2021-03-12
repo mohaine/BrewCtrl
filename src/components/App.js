21028853c4401b8091740cf15b0fb66ec2dda167
Michael Graessle <mike-git@graessle.net>
2021-03-12 08:55:19 -0600

    Cleanups

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,32 @@
 import React, { Component, PropTypes } from 'react'
 import Footer from './Footer'
 import Header from '../containers/Header'
-import { Router, Route, browserHistory } from 'react-router'
+import Default from '../components/Default'
+import Configuration from '../containers/Configuration'
+import Brewery from '../containers/Brewery'
+import StepLists from '../containers/StepLists'
+import About from '../components/About'
 
+import { Switch, Route } from 'react-router-dom'
 
 export default class App extends Component {
+
   render() {
-    const {children} = this.props
-    return  (<div><Header/>
+
+    return (<div><Header />
       <div className="container-fluid">
-        {children}
+
+        <Route path="/brewctrl">
+          <Switch>
+            <Route path="/brewctrl/about"><About /></Route>
+            <Route path="/brewctrl/configuration" ><Configuration /></Route>
+            <Route path="/brewctrl/brew"><Brewery /></Route>
+            <Route path="/brewctrl/steplists"><StepLists /></Route>
+            <Route><Default /></Route>
+          </Switch>
+        </Route>
+
       </div>
-      <Footer/></div>)
+      <Footer /></div>)
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import { Provider } from 'react-redux'
 
 
 import App from './components/App'
-import Default from './components/Default'
-import Configuration from './containers/Configuration'
-import Brewery from './containers/Brewery'
-import StepLists from './containers/StepLists'
-import About from './components/About'
-
-import { Switch, Route , useRouteMatch} from 'react-router-dom'
 import { ConnectedRouter } from 'connected-react-router'
 
 
@@ -27,17 +20,7 @@ store.dispatch({
 render(<div>
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <App>
-        <Route path="/brewctrl">
-          <Switch>
-            <Route path="/brewctrl/about"><About /></Route>
-            <Route path="/brewctrl/configuration" ><Configuration /></Route>
-            <Route path="/brewctrl/brew"><Brewery /></Route>
-            <Route path="/brewctrl/steplists"><StepLists /></Route>
-            <Route><Default /></Route>
-          </Switch>
-        </Route>
-      </App>
+      <App/>
     </ConnectedRouter>
   </Provider>
 
